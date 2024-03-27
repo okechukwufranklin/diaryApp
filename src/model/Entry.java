249cfbd0358ee8e8e1be359234a784c4bed96df9
okechukwufranklin <151369058+okechukwufranklin@users.noreply.github.com>
2024-03-27 14:15:41 +0100

    diary

---
+++ src/model/Entry.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Entry {
+    private  int id;
+    private String title;
+    private String body;
+    private LocalDateTime dateCreated;
+    private String author;
+
+    public Entry(int id,String title,String body,String author) {
+        this.id = id;
+        this.body = body;
+        this.title =title;
+        this.author = author;
+    }
+    public Entry(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
