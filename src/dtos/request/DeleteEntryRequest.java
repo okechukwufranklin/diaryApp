b50700a97684c3b99c5a7fc8e4d03a1c61a977de
okechukwufranklin <151369058+okechukwufranklin@users.noreply.github.com>
2024-03-27 14:13:02 +0100

    diary

---
+++ src/dtos/request/DeleteEntryRequest.java
@@ -0,0 +1,31 @@
+package dtos.request;
+
+public class DeleteEntryRequest {
+    private int  id;
+    private String password;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
